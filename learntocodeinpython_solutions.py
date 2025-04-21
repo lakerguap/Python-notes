2b3fa75939ac243c9ff1cd961123c86b977f0862
rayanbadr <rayan.badr.med@gmail.com>
2025-04-22 00:02:01 +0200

    appunti

---
+++ learntocodeinpython_solutions.py
@@ -0,0 +1,35 @@
+#CH4: Scope
+
+#L1: Scope
+
+def get_max_health(modifier, level):
+    return modifier * level
+
+
+my_modifier = 5
+my_level = 10
+
+
+max_health = get_max_health(my_modifier, my_level)
+
+
+print(f"max_health is: {max_health}")
+
+
+#L2: Global Scope
+
+player_level = 4
+
+
+
+def calculate_health(modifier):
+    return player_level * modifier
+
+
+def calculate_primary_stats(armor_bonus, modifier):
+    return armor_bonus + modifier + player_level
+
+
+print(f"Character has {calculate_health(10)} max health.")
+
+print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
