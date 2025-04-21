c2793b0db46b8b9aaafb5f51864bb34ca9747b62
rayanbadr <rayan.badr.med@gmail.com>
2025-04-22 00:30:17 +0200

    appunti

--- learntocodeinpython_solutions.py
+++ learntocodeinpython_solutions.py
@@ -33,3 +33,23 @@ def calculate_primary_stats(armor_bonus, modifier):
 print(f"Character has {calculate_health(10)} max health.")
 
 print(f"Character has {calculate_primary_stats(3, 8)} primary stats.")
+
+
+
+#L3: Scope Quiz
+
+#Does the get_area_of_circle function have access to the pi variable?
+
+#Yes, because the pi variable is in the global scope
+
+
+#L4: Scope Quiz
+
+#Can the area variable inside the get_area_of_circle function be accessed outside the function?
+
+#No, variables defined inside a function are not accessible outside of it
+
+
+
+
+#CH5: testing and debugging
